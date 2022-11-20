007358fd91822c7bf20d690cc1fd6ad1a9e7fd10
rayhanp1402 <rayhan.hanif14maulana@gmail.com>
2022-11-20 19:55:59 +0700

    input eigenvec jadi matrix numpy

--- src/eigen2.py
+++ src/eigen2.py
@@ -1,6 +1,17 @@
 import numpy as np
 import math
 
+def copy(Matrix):
+    mat = [[0 for j in range(Matrix.shape[0])] for i in range(Matrix.shape[0])]
+
+    for i in range(Matrix.shape[0]):
+        for j in range(Matrix.shape[1]):
+            mat[i][j] = Matrix[i, j]
+
+    return mat
+
+
+
 def matrixMultiplication(Matrix1, Matrix2):
     sum = 0
     resMatrix = [[0 for j in range(len(Matrix1))] for i in range(len(Matrix1))]
@@ -122,7 +133,9 @@ def eigenValue(Matrix): # Note : Call eigenValue(Matrix)[0] to return only the e
 
 
 def eigenVector(Matrix):
-    eigenVecMatrix = eigenValue(Matrix)[1]
+    mat = copy(Matrix)
+
+    eigenVecMatrix = eigenValue(mat)[1]
 
     eigenVecMatrix = np.matrix(eigenVecMatrix)
 
@@ -135,48 +148,51 @@ def eigenVector(Matrix):
 
 # TESTS
 # Samples
-# a = ([[0, 0, -2], 
-#      [1, 2, 1], 
-#      [1, 0, 3]])
+a = ([[0, 0, -2], 
+     [1, 2, 1], 
+     [1, 0, 3]])
 
-# b = [[3, 0],
-#     [8, -1]]
-
-
-# c= [[10, 0, 2],
-#    [0, 10, 4],
-#    [2, 4, 2]]
-
-# d = [[24294.3, 23763.3, -22564, -27522, 2028.72],         
-#     [23763.3, 37215.3, -26584, -31211, -3183.3],         
-#     [-22564, -26584, 32686.9, 25780.3, -9319.5],       
-#     [-27522, -31211, 25780.3, 45872.7, -12919],       
-#     [2028.72, -3183.3, -9319.5, -12919, 23393.1]]
-
-# e = [[2, 1, 0],
-#     [1, 2, 0],
-#     [0, 0, 3],]
-
-# f = [[1, 1, 1, 1, 1],
-#     [16, 8, 4, 2, 1],
-#     [81, 27, 9, 3, 1],
-#     [256, 64, 16, 4, 1],
-#     [625, 125, 25, 5, 1]]
+b = [[3, 0],
+    [8, -1]]
 
 
+c= [[10, 0, 2],
+   [0, 10, 4],
+   [2, 4, 2]]
 
-# # Eigen Values
-# print("Test Eigen Values :")
-# print("Eigen Value Matriks a: ", eigenValue(a)[0])
-# print("Eigen Value Matriks b: ", eigenValue(b)[0])
-# print("Eigen Value Matriks c: ", eigenValue(c)[0])
-# print("Eigen Value Matriks d: ", eigenValue(d)[0])
-# print("Eigen Value Matriks e: ", eigenValue(e)[0])
-# print("Eigen Value Matriks f: ", eigenValue(f)[0], '\n')
+d = [[24294.3, 23763.3, -22564, -27522, 2028.72],         
+    [23763.3, 37215.3, -26584, -31211, -3183.3],         
+    [-22564, -26584, 32686.9, 25780.3, -9319.5],       
+    [-27522, -31211, 25780.3, 45872.7, -12919],       
+    [2028.72, -3183.3, -9319.5, -12919, 23393.1]]
+
+e = [[2, 1, 0],
+    [1, 2, 0],
+    [0, 0, 3],]
+
+f = [[1, 1, 1, 1, 1],
+    [16, 8, 4, 2, 1],
+    [81, 27, 9, 3, 1],
+    [256, 64, 16, 4, 1],
+    [625, 125, 25, 5, 1]]
 
 
 
-# # Eigen Vectors
-# print("Test Eigen Values :")
-# print("Eigen Vector Matriks b:\n", (eigenVector(b)), '\n')
-# print("Eigen Vector Matriks d:\n", (eigenVector(d)), '\n')
+# Eigen Values
+print("Test Eigen Values :")
+print("Eigen Value Matriks a: ", eigenValue(a)[0])
+print("Eigen Value Matriks b: ", eigenValue(b)[0])
+print("Eigen Value Matriks c: ", eigenValue(c)[0])
+print("Eigen Value Matriks d: ", eigenValue(d)[0])
+print("Eigen Value Matriks e: ", eigenValue(e)[0])
+print("Eigen Value Matriks f: ", eigenValue(f)[0], '\n')
+
+
+
+# Eigen Vectors
+b = np.matrix(b)
+d = np.matrix(d)
+
+print("Test Eigen Values :")
+print("Eigen Vector Matriks b:\n", (eigenVector(b)), '\n')
+print("Eigen Vector Matriks d:\n", (eigenVector(d)), '\n')
