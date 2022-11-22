4623d121af8eaed4c177b87973a799409555ca1f
[SulthanDA28] <[13521159@std.stei.itb.ac.id]>
2022-11-22 15:11:37 +0700

    push

--- src/eigendistance.py
+++ src/eigendistance.py
@@ -12,6 +12,7 @@ def eigenVecMax(eigenVecMatrix):
     return (eigenVecMatrix[:, 0])
 
 
+
 def rate(list_photo):
     n1 = list_photo[0]
     row = len(n1)
@@ -20,9 +21,6 @@ def rate(list_photo):
     for i in range(0,len(list_photo)):
         d = np.add(d,list_photo[i])
     rate = d/len(list_photo)
-    for i in range(0,len(rate)):
-        for j in range(0,len(rate[0])):
-            rate[i][j] = math.floor(rate[i][j])
     return rate
 
 def kurangdgnrata2 (list_photo):
@@ -32,11 +30,6 @@ def kurangdgnrata2 (list_photo):
     rata2 = rate(list_photo)
     for i in range(0,len(list_photo)):
         list_photo[i] = np.subtract(list_photo[i],rata2)
-    for i in range(0,len(list_photo)):
-        for baris in range(0,row):
-            for kolom in range(0,col):
-                if(list_photo[i][baris][kolom]<0):
-                    list_photo[i][baris][kolom] = list_photo[i][baris][kolom]*(-1)
     gabung = list_photo[0]
     for i in range(1,len(list_photo)):
         gabung = np.concatenate((gabung,list_photo[i]),axis=1)
@@ -44,8 +37,9 @@ def kurangdgnrata2 (list_photo):
 def eigenface(list_photo):
     kov = kofarian(list_photo)
     eigvec = eigenVector(kov)
+    eigmax = np.transpose(eigvec)
     matrixkrg = kurangdgnrata2(list_photo)
-    hasil = np.matmul(matrixkrg,eigvec)
+    hasil = np.matmul(matrixkrg,eigmax)
     return hasil
 
 
@@ -95,7 +89,9 @@ def splitmatrix(matrix):
 
 
 
-def berat_eigface(eigen_face):
+def berat_eigface(list_photo,eigen_face):
+    krgmatrix = kurangdgnrata2(list_photo)
+    splitkrgmatrix = splitmatrix(krgmatrix)
     eigface = eigen_face
     hasil_akhir=[]
     hasil_split = splitmatrix(eigface)
@@ -103,7 +99,7 @@ def berat_eigface(eigen_face):
     berat = np.zeros(shape=(col,1))
     for j in range(col):
         for k in range(col):
-            d = dotkali(hasil_split[j],hasil_split[k])/dotkali(hasil_split[k],hasil_split[k])
+            d = dotkali(splitkrgmatrix[j],hasil_split[k])/dotkali(hasil_split[k],hasil_split[k])
             berat[k][0] += d
         hasil_akhir.append(berat)
         berat = np.zeros(shape=(col,1))
@@ -134,28 +130,11 @@ def getMinIdx(list):
             break
     return idx
 
-def normalisasi(list):
-    jml = 0
-    panjang = len(list)
-    k = [0 for i in range(panjang)]
-    copy = list
-    for i in range(panjang):
-        k[i] += list[i]**2
-    for i in range(panjang):
-        jml += k[i]
-    jml = jml**(1/2)
-    for i in range(panjang):
-        copy[i] = copy[i]/jml
-    return copy
 
 
 def cek_img(new_img,list_photo,eigen_face,weight_training):
     rata2 = rate(list_photo)
     kurang_img =  np.subtract(new_img,rata2)
-    for j in range(len(new_img[0])):
-        for i in range(len(new_img)):
-            if((kurang_img[i][j])<0):
-                kurang_img[i][j] = kurang_img[i][j]*(-1)
     eigface = eigen_face
     ngesplit = splitmatrix(eigface)
     berat_training=weight_training
@@ -166,17 +145,16 @@ def cek_img(new_img,list_photo,eigen_face,weight_training):
         berat_training[i] = np.subtract(berat_img,berat_training[i])
     for i in range(len(ngesplit)):
         berat_training[i] = panjangmatrix(berat_training[i])
-    hasil_akhir = normalisasi(berat_training)
-    return hasil_akhir
+    return berat_training
     
     
 
         
 
 
-a = np.array([[2,0,1],
-     [1,2,0],
-     [0,2,4]])
+# a = np.array([[2,0,1],
+#      [1,2,0],
+#      [0,2,4]])
 
 # b = np.array([[1,1,1],
 #      [0,1,0],
@@ -211,15 +189,6 @@ a = np.array([[2,0,1],
 # berat = berat_eigface(d)
 # print(berat)
 
-eigv = (eigenVector(a))
-print(eigenVecMax(eigv))
-
-        
-
-
-
-
-
-
-
-
+# print(eigenValue((a))[0])
+# print(eigenVector(a))
+# print(searchMax(eigenValue(a)[0], eigenVector(a)))
