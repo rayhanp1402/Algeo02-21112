0940a47073c6800469e60c8002a6c7b61c8e2f09
[SulthanDA28] <[13521159@std.stei.itb.ac.id]>
2022-11-22 14:41:14 +0700

    update kofarian

--- src/kofarian.py
+++ src/kofarian.py
@@ -11,21 +11,13 @@ def kofarian(list_photo):
   for i in range(0,len(list_photo)):
     d = np.add(d,list_photo[i])
   rate = d/len(list_photo)
-  for i in range(0,len(rate)):
-    for j in range(0,len(rate[0])):
-      rate[i][j] = math.floor(rate[i][j])
   for i in range(0,len(list_photo)):
     list_photo[i] = np.subtract(list_photo[i],rate)
-  for i in range(0,len(list_photo)):
-    for baris in range(0,row):
-      for kolom in range(0,col):
-        if(list_photo[i][baris][kolom]<0):
-          list_photo[i][baris][kolom] = list_photo[i][baris][kolom]*(-1)
   gabung = list_photo[0]
   for i in range(1,len(list_photo)):
     gabung = np.concatenate((gabung,list_photo[i]),axis=1)
   trnsps = np.transpose(gabung)
-  L = np.matmul(trnsps,gabung)
+  L = np.dot(trnsps,gabung)
   return L
 
 # k,l = extractor_data('..\Algeo02-21112\test\dataset')
